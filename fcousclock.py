58ed2d9a0d6326e34a17652ad276ee53a0f88b98
c&fang <105961336+Unisr@users.noreply.github.com>
2023-07-30 12:51:43 +0800

    Update and rename README.md to fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,12 @@
+import time
+
+def count_down(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        minutes, seconds = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(minutes, seconds)
+        print(timer, end='\r')
+        time.sleep(1)
+        seconds -= 1
+    
+    print("Time up!")
